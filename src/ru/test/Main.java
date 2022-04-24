d194d0bfb9b44f8ad0b855240ab471a42ce1cd61
daniil <trostin1998@bk.ru>
2022-04-24 22:52:09 +0300

    HW 5 done

--- src/ru/test/Main.java
+++ src/ru/test/Main.java
@@ -3,6 +3,44 @@ package ru.test;
 public class Main {
 
     public static void main(String[] args) {
+        // Task 1
+        int[] a = new int[]{15, 2, 19};
+        float[] b = {1.57f, 7.654f, 9.986f};
+        int[] c = {5, 10, 15};
+        //Task 2
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + ",");
 
+        }
+        System.out.println("\n");
+        for (int i = 0; i < b.length; i++) {
+            System.out.print(b[i] + ",");
+        }
+        System.out.println("\n");
+        for (int i = 0; i < c.length; i++) {
+            System.out.print(c[i] + ",");
+        }
+        System.out.println("\n");
+        // Task3
+        for (int i = a.length - 1; i > -1; i--) {
+            System.out.print(a[i] + ",");
+        }
+        System.out.println("\n");
+        for (int i = b.length - 1; i > -1; i--) {
+            System.out.print(b[i] + ",");
+        }
+        System.out.println("\n");
+        for (int i = c.length - 1; i > -1; i--) {
+            System.out.print(c[i] + ",");
+        }
+        System.out.println("\n");
+        // Task 4
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] % 2 == 0) {
+            } else {
+                a[i] = a[i] + 1;
+            }
+            System.out.print(a[i] + " ");
+        }
     }
 }
